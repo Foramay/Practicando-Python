4be82996a0db095fba300e04e6ea99c8a49ca402
David <davidforastier10@gmail.com>
2023-02-25 19:29:21 -0300

    Condicional 13

--- Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales13.py
+++ Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales13.py
@@ -12,3 +12,18 @@ Kinesiología		20%
 
 Obtener la cantidad de dinero que recibirá cada área, para cualquier monto presupuestal 		que se ingrese por teclado.
 """
+
+presupuesto_anual = input("Ingresá el presupuesto anual: $")
+
+if presupuesto_anual.isdigit():
+    total_pedriatria = 60 * int(presupuesto_anual) // 100
+    total_traumatologia = 20 * int(presupuesto_anual) // 100
+    total_kinesiologia = 20 * int(presupuesto_anual) // 100    
+    print(f"""
+El monto total por año es de === ${presupuesto_anual}
+El monto que recibirá el área de Pedriatría es de === ${float(total_pedriatria)}
+El monto que recibirá el área de Traumatología es de === ${float(total_traumatologia)}
+El monto que recibirá el área de Kinesiología es de === ${float(total_kinesiologia)}
+    """)
+else:
+    print("No")
