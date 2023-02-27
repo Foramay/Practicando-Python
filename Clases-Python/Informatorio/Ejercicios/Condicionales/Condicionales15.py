544e0ef543a8a63649dcf6b83dc21e0ca73babc5
David <davidforastier10@gmail.com>
2023-02-27 11:41:06 -0300

    Condicional 15 y 16

--- Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales15.py
+++ Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales15.py
@@ -1,3 +1,19 @@
 """
 Determinar si un alumno aprueba a reprueba un curso, sabiendo que aprobara si su promedio de tres calificaciones es mayor o igual a 70; desaprueba en caso contrario.
 """
+
+calificacion1 = input("Ingresá la primer nota: ")
+calificacion2 = input("Ingresá la segunda nota: ")
+calificacion3 = input("Ingresá la tercer nota: ") 
+
+
+if calificacion1.isdigit() and calificacion2.isdigit() and calificacion3.isdigit():
+    promedio = int(calificacion1) + int(calificacion2) + int(calificacion3) // 3
+    if int(promedio) >= 7:
+        print(f"Estás aprobado. El resultado de tus calificaciones es de {promedio}")
+    else:
+        print("Estás desaprobado.")
+else:
+    print("Por favor ingresá la nota.") 
+
+
--- Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales16.py
+++ Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales16.py
@@ -1,3 +1,23 @@
 """
 Hacer un programa que imprima el nombre de un artículo, clave, precio original y su precio con descuento. El descuento lo hace en base a la clave, si la clave es 01 el descuento es del 10% y si la clave es 02 el descuento en del 20% (solo existen dos claves).
 """
+
+
+
+nombre_articulo = input("Ingresá el nombre del artículo: ")
+precio_original = input(f"Ingresá el precio de {nombre_articulo}: $")
+clave_descuento = input("Ingresá la clave de descuento: ")
+
+
+if clave_descuento == "01":
+    clave_1 = int(precio_original) * 10 // 100
+    total_descuento = int(precio_original) - int(clave_1)
+    print(f"El total a pagar por el artículo {nombre_articulo} es de ${total_descuento}")
+elif clave_descuento == "02":
+    clave_2 = int(precio_original) * 20 // 100
+    total_descuento = int(precio_original) - int(clave_2)
+    print(f"El total a pagar por el artículo {nombre_articulo} es de ${total_descuento}")
+else:
+    print(f"""No hay descuento para este artículo.
+El total a pagar es de ${precio_original}
+    """) 
