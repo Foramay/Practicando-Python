260e9f13befe5b4dd57483d79a51511eb5793a88
David <davidforastier10@gmail.com>
2023-02-27 12:26:21 -0300

    Condicional 18

--- Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales17.py
+++ Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales17.py
@@ -1,3 +1,33 @@
 """
 En un supermercado se hace una promoción, mediante la cual el cliente obtiene un descuento dependiendo de un número que se escoge al azar. Si el numero escogido es menor que 74 el descuento es del 15% sobre el total de la compra, si es mayor o igual a 74 el descuento es del 20%. Obtener cuánto dinero se le descuenta.
 """
+
+print("Bienvenido.")
+numero_azar = input("Elegí un número al azar: ")
+
+
+if numero_azar.isdigit():
+    if int(numero_azar) < 74:
+        compra = input("Ingresá el monto total de la compra: $")
+        if compra.isdigit():
+            descuento = int(compra) * 15 // 100
+            total = int(compra) - int(descuento)
+            print(f"""El descuento es del 15%
+El monto total de la compra es de ${float(compra)}
+El total a pagar es de ${float(total)}
+            """)
+        else:
+            print("Ingresá el monto de la compra.")
+    elif int(numero_azar) >= 74:
+        compra = input("Ingresá el monto total de la compra: $")
+        if compra.isdigit():
+            descuento = int(compra) * 20 // 100
+            total = int(compra) - int(descuento)
+            print(f"""El descuento es del 20%
+El monto total de la compra es de ${float(compra)}
+El total a pagar es de ${float(total)}
+            """)
+        else:
+            print("Ingresá el monto de la compra.")
+else:
+    print("Elegí un número al azar.")
--- Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales18.py
+++ Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales18.py
@@ -1,3 +1,23 @@
 """
 Leer 2 números; si son iguales que los multiplique, si el primero es mayor que el segundo que los reste y si no que los sume.
 """
+
+numero1 = input("Ingresá un número: ")
+numero2 = input("Ingresá otro número: ")
+
+
+if numero1.isdigit() and numero2.isdigit():
+    if numero1 == numero2:
+        operacion = "multiplicación"
+        resultado = int(numero1) * int(numero2)
+    elif numero1 > numero2:
+        operacion = "resta"
+        resultado = int(numero1) - int(numero2)
+    else:
+        operacion = "suma"
+        resultado = int(numero1) + int(numero2)
+    print(f"""La operacion que se ejecutó es una {operacion}.
+El resultado de la operación es {resultado}
+    """)
+else:
+    print("Ingresá números para hacer la operación.")
