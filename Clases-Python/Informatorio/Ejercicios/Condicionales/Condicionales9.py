9338117d3e671f815d4e533ccdccfade0e53069b
David <davidforastier10@gmail.com>
2023-02-28 11:10:07 -0300

    Condicional 9

--- Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales9.py
+++ Clases-Python/Informatorio/Ejercicios/Condicionales/Condicionales9.py
@@ -1,3 +1,16 @@
 """
 Realizar un programa que sea capaz de, habiéndose ingresado dos números m y n, determine si n es divisor de m.
 """
+
+
+numero1 = input("Ingresá el primer número: ")
+numero2 = input("Ingresá el segundo número: ")
+
+
+if numero1.isdigit() and numero2.isdigit():
+    if int(numero1) % int(numero2) == 0:
+        print("Si es divisor")
+    else:
+        print("No es divisor")
+else:
+    print("Ingresá números validos.")
