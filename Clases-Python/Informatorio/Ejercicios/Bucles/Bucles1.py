53e49751da24fbfaf152ddaaa50219d93743b49f
David <davidforastier10@gmail.com>
2023-03-01 10:07:24 -0300

    Bucles 1

--- Clases-Python/Informatorio/Ejercicios/Bucles/Bucles1.py
+++ Clases-Python/Informatorio/Ejercicios/Bucles/Bucles1.py
@@ -5,3 +5,24 @@ a) Te toca escribir un programa que simule el proceso de Login. Para ello el pro
 
 b) Modificar el programa anterior para que solamente permita una cantidad fija de intentos. 
 """
+
+
+#Desafío forma 1
+CONTRASEÑA = "asd"
+contraseña = input("Ingresá la contraseña: ")
+while contraseña != CONTRASEÑA:
+        print("Contraseña incorrecta. Volvé a escribir")
+        contraseña = input("Ingresá la contraseña: ")        
+print("Contraseña correcta.")
+
+
+#Desafío forma 2
+"""
+for i in range(3):
+    contraseña = input("Ingresá tu contraseña: ")
+    if contraseña != CONTRASEÑA:
+        print("Contraseña incorecta.")
+    else:
+        print("Contraseña correcta.")
+        break
+"""
