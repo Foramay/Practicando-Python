778f2723ffc31b1cce5aea50f2e800ad37a30da9
David <davidforastier10@gmail.com>
2023-03-04 15:20:06 -0300

    Bucles 6

--- Clases-Python/Informatorio/Ejercicios/Bucles/Bucles6.py
+++ Clases-Python/Informatorio/Ejercicios/Bucles/Bucles6.py
@@ -1,3 +1,12 @@
 """
 Determinar el número mayor de 10 números ingresados.
 """
+
+
+numero_mayor = 0
+print("Bienvenido")
+for i in range(10):
+    numero = input("Ingresá el número: ")
+    if int(numero) > int(numero_mayor):
+        numero_mayor = int(numero)
+print( f"El número mayor es {numero_mayor}")
