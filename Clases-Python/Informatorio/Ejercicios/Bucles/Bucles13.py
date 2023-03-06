7870775c6a801aec06eb011b320f6598b65cc37a
David <davidforastier10@gmail.com>
2023-03-06 16:41:10 -0300

    Bucles 13

--- Clases-Python/Informatorio/Ejercicios/Bucles/Bucles13.py
+++ Clases-Python/Informatorio/Ejercicios/Bucles/Bucles13.py
@@ -1,3 +1,18 @@
 """
 Diseñar un programa que permita calcular el total de una compra, ingresando cantidad y precio de los productos. El programa debe estar preparado para que el ingreso de los datos se produzca hasta que el usuario lo desee.
 """
+
+print("Bienvenido.")
+
+suma = 0
+while True:
+    producto = input("Ingresá el producto: ")
+    precio_producto = input(f"Ingresá el precio del {producto}: $")
+    cantidad_producto = input(f"Ingresá la cantidad de {producto}: ")
+
+    suma = int(suma) + int(precio_producto) * int(cantidad_producto)
+
+    seguir = input("Desea agregar más productos?: si/no ")
+    if seguir.lower() == "no":
+        break
+print(f"El total a pagar es ${suma}")
