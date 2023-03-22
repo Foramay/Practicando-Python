c5d0bca869df59249936ae3911e956cf26933487
David <davidforastier10@gmail.com>
2023-03-21 21:58:52 -0300

    listas, tuplas y diccionarios 2

--- Clases-Python/Informatorio/Ejercicios/Listas_Tuplas_Diccionarios/Listas_Tuplas_Diccionarios2.py
+++ Clases-Python/Informatorio/Ejercicios/Listas_Tuplas_Diccionarios/Listas_Tuplas_Diccionarios2.py
@@ -1,3 +1,25 @@
 """
-Crea un diccionario donde la clave sea el nombre de biólogos y el valor sea el email (no es necesario validar). Tendrás que ir pidiendo contactos hasta el usuario diga que no quiere insertar mas. No se podrán insertar nombres repetidos.
+Crea un diccionario donde la clave sea el nombre de biólogos y el valor sea el email (no es necesario validar). Tendrás que ir pidiendo contactos hasta que el usuario diga que no quiere insertar mas. No se podrán insertar nombres repetidos.
 """
+
+
+diccionario = {}
+
+while True:
+    nombre = input("Ingresá tu nombre: ")
+    gmail = input("Ingresá un gmail: ")
+    
+    if nombre.isalnum() and gmail.isalnum():
+        diccionario.setdefault(nombre, gmail)
+    else:
+        print("Ingresá algo válido.")
+
+
+    agregar_mas_contactos = input("¿Querés agregar otro contacto? si/no: ")
+
+
+    if agregar_mas_contactos.lower() == "no" or agregar_mas_contactos.lower() != "si":
+        print("Fin del programa")
+        break
+
+print(diccionario)
