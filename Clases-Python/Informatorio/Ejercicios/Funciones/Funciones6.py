a3f366e32a7f1d8d55e6659b01ad94f44655fd5f
David <davidforastier10@gmail.com>
2023-05-18 10:47:20 -0300

    Ejercicio Funciones 6

--- Clases-Python/Informatorio/Ejercicios/Funciones/Funciones6.py
+++ Clases-Python/Informatorio/Ejercicios/Funciones/Funciones6.py
@@ -7,3 +7,18 @@ Escriba una función que tome tres números como parámetros y devuelva el valor
 
 Sugerencia: El valor medio es el medio de los tres valores cuando se ordenan en orden ascendente. Se puede encontrar usando declaraciones if, o con un poco de creatividad matemática.
 """
+
+LISTA = []
+cont = 1
+def obtener_valor_medio(*args):
+    #*args sería un tupla
+    #Iteramos por cada elemento de la tupla
+    for numero in args:
+        #Y agregamos el elemento a una lista
+        LISTA.append(numero)
+    #Acá ordenamos la lista de forma ascendente con el metodo sort()
+    LISTA.sort()
+    #Y finalmente mostramos por pantalla el resultado del medio ingresando al elemento
+    print(f"El número intermedio es {LISTA[cont]}")  
+
+obtener_valor_medio(109, 19, 5)
